f91bc67be137616f58e247fe3def2bbb91adfba7
mona <jalal@wisc.edu>
2016-05-27 00:36:50 -0500

    IntelliJ IDEA debugger soooo mad at me :(

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -10,11 +10,7 @@ public static TreeMap<Integer, Character> reverse(TreeMap<Character,Integer> tma
 /**
  * Created by mona on 5/26/16.
  */
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Collections;
+import java.util.*;
 
 public class IsomorphicStrings {
     //the words "abca" and "zbxz" are isomorphic
@@ -26,53 +22,42 @@ public class IsomorphicStrings {
     //assuming two isomorphic strings are of the same length
 
     public static boolean areIsomorphic(String s1, String s2) {
-        Map<Character, Integer> freqMap1 = new HashMap<>();
-        Map<Character, Integer> freqMap2 = new HashMap<>();
+        Map<Character, ArrayList<Integer>> freqMap1 = new HashMap<>();
+        Map<Character, ArrayList<Integer>> freqMap2 = new HashMap<>();
 
         for (int i=0; i<s1.length(); i++) {
             if (freqMap1.containsKey(s1.charAt(i))) {
-                int freq1 = freqMap1.get(s1.charAt(i));
-                freqMap1.put(s1.charAt(i), freq1 + 1);
+                freqMap1.get(s1.charAt(i)).add(i);
             } else {
-                freqMap1.put(s1.charAt(i), 1);
+                freqMap1.put(s1.charAt(i), new ArrayList<>(Arrays.asList(i)));
             }
 
         }
 
         for (int i=0; i<s2.length(); i++) {
             if (freqMap2.containsKey(s2.charAt(i))) {
-                int freq2 = freqMap2.get(s2.charAt(i));
-                freqMap2.put(s2.charAt(i), freq2 + 1);
+                freqMap2.get(s2.charAt(i)).add(i);
+            } else {
+                freqMap2.put(s2.charAt(i), new ArrayList<>(Arrays.asList(i)));
             }
-            else {
-                freqMap2.put(s2.charAt(i), 1);
-            }
-
         }
 
-        List<Integer> freqList1 = new ArrayList<>();
-        List<Integer> freqList2 = new ArrayList<>();
+        Collection<ArrayList<Integer>> freqList1;
+        freqList1 = freqMap1.values();
+        Collection<ArrayList<Integer>> freqList2;
+        freqList2 = freqMap2.values();
 
-        for (Map.Entry entry: freqMap1.entrySet()) {
-            freqList1.add((Integer) entry.getValue());
-        }
 
-        for (Map.Entry entry: freqMap2.entrySet()) {
-            freqList2.add((Integer) entry.getValue());
-        }
-
-        Collections.sort(freqList1);
-        Collections.sort(freqList2);
+        //Collections.sort(freqList1);
+       // Collections.sort(freqList2);
 
         return freqList1.equals(freqList2);
 
     }
 
-    //current algorithm fails with "aba" and "baa" given as input
-
     public static void main(String[] args) {
-        String s1="abca";
-        String s2="zbxz";
+        String s1="abb";
+        String s2="aba";
         System.out.println(areIsomorphic(s1, s2));
     }
 }
