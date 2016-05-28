481d49e73fdea13c6102e3a44296fdf8efa8781e
mona <jalal@wisc.edu>
2016-05-28 02:07:33 -0500

    BFS tested

--- BreathFirstSearch.java
+++ BreathFirstSearch.java
@@ -4,7 +4,7 @@
 
 import java.util.LinkedList;
 import java.util.Queue;
-
+//add code later on to save the path from root to dest in an array
 public class BreathFirstSearch {
     public static void BFS(GraphNode root, int num) {
         if (root.val == num) {
@@ -13,24 +13,29 @@ public class BreathFirstSearch {
         Queue<GraphNode> queue = new LinkedList<>();
         queue.add(root);
         root.visited = true;
-        for (GraphNode g : queue.remove().neighbors) {
-            if (g.val == num) {
-                System.out.println("Node "+g+" contains "+num);
+        while (!queue.isEmpty()) {
+            for (GraphNode g : queue.remove().neighbors) {
+                if (!g.visited) {
+                    System.out.println(g);
+                    if (g.val == num) {
+                        System.out.println("Node contains " + num);
+                    }
+                    g.visited = true;
+                    queue.add(g);
+                }
             }
-            g.visited = true;
-            queue.add(g);
         }
     }
 
-    public static void main(String args) {
+    public static void main(String[] args) {
         GraphNode n1 = new GraphNode(1);
         GraphNode n2 = new GraphNode(2);
         GraphNode n3 = new GraphNode(3);
         GraphNode n4 = new GraphNode(4);
         GraphNode n5 = new GraphNode(5);
 
-        n1.neighbors = new GraphNode[] {n2, n3};
-        n2.neighbors = new GraphNode[] {n3, n4, n5};
+        n1.neighbors = new GraphNode[] {n2, n4};
+        n2.neighbors = new GraphNode[] {n4, n5};
         n3.neighbors = new GraphNode[] {n4};
         n4.neighbors = new GraphNode[] {n1, n5};
         n5.neighbors = new GraphNode[] {n3, n4};
